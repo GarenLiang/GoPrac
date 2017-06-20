71997ac5aabf86fa11e0238053dc81902d3aa99c
GarenLiang <ideabreeze@gmail.com>
2017-06-20 16:14:30 -0700

    var and string

---
+++ number.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+  a := 61
+  b := 5
+
+  fmt.Println("Sum", a+b)
+  fmt.Println("Subdivide", a-b)
+  fmt.Println("Multiply", a*b)
+  fmt.Println("Divide", a/b)
+  fmt.Println("Modulus", a%b)
+}
