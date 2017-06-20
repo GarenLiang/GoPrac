bba8548e0131931a4d11c8f5f239cc391bec1eef
GarenLiang <ideabreeze@gmail.com>
2017-06-20 16:34:27 -0700

    Scope

---
+++ Boolean.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+
+func main() {
+  inGame := true
+  someBoolean := 5==3
+
+  fmt.Println(inGame)
+  fmt.Println(someBoolean)
+}
---
+++ scope.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+var iron int = 26
+
+func main(){
+  gold := 79
+  fmt.Println(iron)
+  fmt.Println(gold)
+}
+func anotherFunction() {
+  fmt.Println(iron)
+  // fmt.Println(gold)
+}
