758b349babd03d967e25b958abcb25891b58f7b5
GarenLiang <ideabreeze@gmail.com>
2017-06-20 15:48:54 -0700

    Go Programming

---
+++ variable.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+  var myAge int = 21
+  myVariable := 34
+
+  // myAge, myVariable, anotherOne := 36, 83, 75
+
+  fmt.Println(myAge)
+  fmt.Println(myVariable)
+
+  fmt.Println("--------")
+
+  myAge = 37
+  myVariable = 765
+
+  fmt.Println(myAge)
+  fmt.Println(myVariable)
+}
